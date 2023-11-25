e6b63ee36d28621eb01034ae3df89193b8947c08
samina265 <syedasaminahussain552@gmail.com>
2023-11-25 11:19:15 +0600

    prb Fixed

---
+++ basic.py
@@ -0,0 +1,175 @@
+# print (4+5)
+# print('samina')
+# print('goru')
+# print('das')
+
+# x =2
+# print (x+3)
+# y = 3
+# print(x+y)
+# x = 9
+# print(x+y)
+
+# print (x+3)
+# print( _ + y)
+
+# name = 'youtube'
+# print(name)
+# print(name + 'rocks')
+# print(name[0])
+# print(name[3])
+# print(name[-1])
+# print(name[-2])
+# print(name[-7])
+# print(name[1 : 4])
+# print(name[:4])
+# print(name[1:])
+# print(name[3:10])
+# # name[0:3] = 'my'
+# # print(name)
+# print('my '+ name[3:])
+# myname = 'Syeda Samina'
+# print(len(myname))
+
+# nums = [24, 56,78 ,35, 13]
+# print(nums)
+# print(nums[0])
+# print(nums[2:])
+# print(nums[-1])
+# print(nums[-5])
+# names = ['samina', 'sajjad','jon']
+# values = [9.5, 'Samima',24]
+# mil = [nums, names]
+# print(mil)
+# nums.append(45)
+# print(nums)
+# nums.insert(2,4)
+# print(nums)
+# nums.remove(13)
+# print(nums.pop(1))
+# print(nums.pop())
+# print(nums)
+# del nums[2:]
+# nums.extend([46,79,55,21])
+# print(nums)
+# print(min(nums))
+# print(max(nums))
+# print(sum(nums))
+# nums.sort()
+# print(nums)
+
+# print('i am \'a\"good girl"\n he is a good boy')
+# print("hey",6,7, sep=' ! ',end="00\n")
+# print("Samina")
+
+
+# a= 12
+# b= complex(2,3)
+# c= True
+# d= 12.6
+# e= 'sami'
+# f=12.78956755
+# print("type of a: ",type(a))
+# print("type of a: ",type(b))
+# print("type of a: ",type(c))
+# print("type of a: ",type(d))
+# print("type of a: ",type(e))
+
+# a= input()
+# b= int(input())
+# print(int(a)+b)
+
+# c=1.9
+# d=4
+# print(c+d)
+
+# str="""Samina is passonate if
+# she serve the country that will be prestigius for us.MS Samina you ar imaging"""
+# print(str)
+
+# fruit='Mango'
+# print(fruit[0:-3])
+# print(fruit[:-3])
+# print(fruit[-3:])
+# print(fruit[-3:-1])
+# print(fruit[-3:len(fruit)-1])
+# print(fruit[-1:-3])
+
+# a= "!!!Samina !!!!! Sajjad"
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.replace("Samina",'ruly'))
+# print(a.split(" "))
+
+# blogHeading = "introduction to js"
+# print(blogHeading.title())
+# print(blogHeading.capitalize())
+# st="welcome to the Console!!"
+# print(len(st))
+# print(st.center(50))
+# print(len(st.center(50)))
+
+# #list
+# marks=[3,7,8,"samina","sajjad",2, 6, 7, 'rafa', 'abba']
+# #in keyword
+# if 6 in marks:
+#   print("yes")
+# else:
+#   print("no")
+
+# if "samina" in marks:
+#   print("yes")
+# else:
+#   print("no")
+
+# print(marks)
+# print(marks[1:-1:2])
+
+# #append
+# marks.append([2,6,7,"rafa","abba"])
+# print(marks)
+# print(marks[1:9])
+# print(marks[1:9:3])
+# lst=[i*i for i in range(10)]
+# print(lst)
+
+# lst=[i-i for i in range(10)]
+# print(lst)
+# #list can override
+# lst=[i*i for i in range(10)
+# if i%2==0]
+# print(lst)
+#list imp method
+# li = [11,45,1,2,3,4,5,1,1,1]
+# li.append(9)
+# li.sort()
+# li.sort(reverse=True)
+# print(li.index(1))
+# print(li.count(1))
+# m=li.copy()
+# m[0]=0
+# print(m)
+# print(li)
+# li.insert(3,899)
+# m = [900, 1000, 1100]
+# k=li+m
+# print(k)
+# # li.extend(m)
+# print(li)
+
+#tuple
+# tup=(1)
+# print(type(tup),tup)
+# tup=(1,2,5,7,9)
+# print(type(tup),tup)
+# print(len(tup))
+# print(tup[0])
+# print(tup[-1])
+
+# if 5 in tup:
+#   print("yes")
+
+# tup2=tup[1:4]
+# print(type(tup2),tup2)
